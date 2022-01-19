0afdaad757f4ea313c0d7cee365a3466f6ca5bca
Devdarji <darjidevendra3@gmail.com>
2022-01-19 07:27:50 +0530

    Pattern Programs Added

---
+++ pattern18.py
@@ -0,0 +1,10 @@
+# AAAAA
+# BBBB
+# CCC
+# DD
+# E
+
+for i in range(65, 70):
+    for j in range(69, i-1, -1):
+        print(chr(i), end="")
+    print()
---
+++ pattern19.py
@@ -0,0 +1,10 @@
+# ABCDE
+# ABCD
+# ABC
+# AB
+# A
+
+for i in range(69, 64, -1):
+    for j in range(65, i+1):
+        print(chr(j), end="")
+    print()
